0977bbe488b6c421f49d8e092ba7fa9975628d11
vTh3FaLL3n <prl96@yahoo.com>
2018-02-20 20:53:11 -0500

    current updates

---
+++ public/assets/javascript/crm.js
@@ -0,0 +1,7 @@
+//clicks to switch between pages on dashboard//
+$("#alldandf_nav").on("click", function(){
+    window.location.replace("deadlineandflightdashboard.html");
+  });
+  $("#allclients_nav").on("click", function(){
+    window.location.replace("maindashboard.html");
+  })
