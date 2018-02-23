15036a21f7e6830cc3e6a6e9b71016767239c435
Michel B <michelb@JB-MBA.local>
2018-02-22 21:39:26 -0500

    upcoming deadlines and flights added to orm

--- controllers/clientsController.js
+++ controllers/clientsController.js
@@ -8,6 +8,7 @@ var client = require("../models/clients.js");
 //   res.redirect("/clients");
 // });
 
+// WORKING HTML Route-- Gets and displays all clients
 router.get("/clientlist", function(req, res) {
   client.all(function(clientData) {
       console.log(clientData);
@@ -15,47 +16,47 @@ router.get("/clientlist", function(req, res) {
   });
 });
 
-// WORKING
+// WORKING API Route-- Gets all clients
 router.get("/api/clients", function(req, res) {
   client.all(function(clientData) {
     res.json(clientData);
   });
 });
 
-//html route
+// NOT WORKING YET HTML Route -- Gets a specific client by ID
 router.get("/client/:id", function(req, res){
   client.findOne(req.params.id,function(data){
     res.render("maindashboard", {client_data: data});
   });
 });
 
-//api route
+// WORKING API Route -- Gets a specific client by ID
 router.get("/api/client/:id", function(req, res){
   client.findOne(req.params.id,function(data){
     res.json(data);
   });
 });
 
-// post route -> back to index
+// NOT WORKING YET HTML Route -- Creates a new client object to add to db
 router.post("/clients/create", function(req, res) {
   // takes the request object using it as input for buger.addBurger
   client.create((req.body.client_name, req.body.primary_contact, req.body.primary_phone, req.body.primary_email, req.body.preferred_dep_airport, req.body.payment_type), function(result) {
     // wrapper for orm.js that using MySQL insert callback will return a log to console,
     // render back to index with handle
     console.log(result);
-    res.redirect("/");
+    res.redirect("/clientpage");
   });
 });
 
 // put route -> back to index
-router.put("/clients/:id", function(req, res) {
-  client.update(req.params.id, function(result) {
-    // wrapper for orm.js that using MySQL update callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    // Send back response and let page reload from .then in Ajax
-    res.sendStatus(200);
-  });
-});
+// router.put("/clients/:id", function(req, res) {
+//   client.update(req.params.id, function(result) {
+//     // wrapper for orm.js that using MySQL update callback will return a log to console,
+//     // render back to index with handle
+//     console.log(result);
+//     // Send back response and let page reload from .then in Ajax
+//     res.sendStatus(200);
+//   });
+// });
 
 module.exports = router;
