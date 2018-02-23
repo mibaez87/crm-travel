9619881f8179218138cdfbf7ac131f46b9a100b1
Michel B <michelb@JB-MBA.local>
2018-02-22 20:18:12 -0500

    working endpoints for today's deadlines and today's flights

--- models/flights.js
+++ models/flights.js
@@ -1,30 +1,36 @@
 var orm = require("../config/orm.js");
 
 var flight = {
-    all: function(cb) {
-      orm.all("trips", function(res) {
-        cb(res);
-      });
-    },
-    findOne: function(dep_date, cb){
-      orm.findOne("trips", "dep_date", dep_date, cb);
-    },
-    findMultiple: function(cb){
-        orm.findMultiple("trips", ["client_id", "deposit_due", "cancellation_deadline", "final_payment"], cb);
-    },
-    create: function(name, cb) {
-      orm.create("trips", [
-        "client_name", "primary_contact", "primary_phone", "primary_email", "preferred_dep_airport", "payment_type", "active"
-      ], [
+  all: function (cb) {
+    orm.all("trips", function (res) {
+      cb(res);
+    });
+  },
+  findOne: function (dep_date, cb) {
+    orm.findOne("trips", "dep_date", dep_date, cb);
+  },
+  findMultiple: function (cb) {
+    orm.findMultiple("trips", ["client_id", "deposit_due", "cancellation_deadline", "final_payment"], cb);
+  },
+  getToday: function (deposit, cancel, final, cb) {
+    orm.getToday("trips", ["client_id", "deposit_due", "cancellation_deadline", "final_payment"], "deposit_due", "cancellation_deadline", "final_payment", deposit, cancel, final, cb);
+  },
+  getTodayFlights: function (departure, arrival, cb) {
+    orm.getTodayFlights("trips", ["client_id", "dep_flight", "dep_airport", "dep_time", "arr_flight", "arr_airport", "arr_time"], "dep_date", "arr_date", departure, arrival, cb);
+  },
+  create: function (name, cb) {
+    orm.create("trips", [
+      "client_name", "primary_contact", "primary_phone", "primary_email", "preferred_dep_airport", "payment_type", "active"
+    ], [
         name, contact, phone, email, depAir, payment, true
       ], cb);
-    },
-    update: function(id, cb) {
-      var condition = "id=" + id;
-      orm.update("trips", {
-        active: false
-      }, condition, cb);
-    }
-  };
-  
-  module.exports = flight;
+  },
+  update: function (id, cb) {
+    var condition = "id=" + id;
+    orm.update("trips", {
+      active: false
+    }, condition, cb);
+  }
+};
+
+module.exports = flight;
