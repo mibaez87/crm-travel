fc452818f602affd3fcb6bb0800166245d3f99ce
vTh3FaLL3n <prl96@yahoo.com>
2018-02-22 20:09:47 -0500

    moving from page to page code

--- public/assets/javascript/crm.js
+++ public/assets/javascript/crm.js
@@ -1,5 +1,26 @@
+$(document).ready(function(){
+  $('.ui.accordion').accordion();
+  $('.ui.dropdown').dropdown();
 
-$('.ui.accordion')
-  .accordion()
-;
+});
+$("#newClientSub").click(function(){
+  window.location.href = "clientpage.html";
+    return false;
+});
+$("#maindash_nav").click(function(){
+  window.location.href = "maindashboard.html";
+    return false;
+});
+$("#allclients_nav").click(function(){
+  window.location.href = "clientlist.html";
+    return false;
+});
+$("#addnewclient").click(function(){
+  window.location.href = "newclient.html";
+    return false;
+});
+$("#alldandf_nav").click(function(){
+  window.location.href = "deadlineandflightdashboard.html";
+    return false;
+});
 
