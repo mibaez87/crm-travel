db1cc06f1d48503d0fe3f699aef0dcd74c4260b2
Michel B <michelb@JB-MBA.local>
2018-02-23 23:46:05 -0500

    corrected connection

--- config/connection.js
+++ config/connection.js
@@ -9,11 +9,7 @@ if (process.env.JAWSDB_URL) {
     user: "root",
     password: "",
     database: "clients_db"
-<<<<<<< HEAD
   })
-=======
-  });
->>>>>>> master
 };
 
 connection.connect(function (err) {
